8a1ac3497240e4a5527fb0b9358540d5a7e039dc
VinodDevOpsTech <vinodkumar160107@gmail.com>
2026-05-22 16:44:01 +0530

    hello

--- vinod.sh
+++ vinod.sh
@@ -1,1 +1,7 @@
-echo "hello world
+#! /bin/bash
+# This is a simple script to print the current date and time
+echo "Current date and time: $(date)"
+# This is a simple script to print the current working directory
+echo "Current working directory: $(pwd)"
+# This is a simple script to list the files in the current directory
+echo "Files in the current directory:"
