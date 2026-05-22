b8b0fde04a4dc21517f29247882eda7cc61f3ee3
VinodDevOpsTech <vinodkumar160107@gmail.com>
2026-05-22 18:41:02 +0530

    time calculation

--- 01_special_var.sh
+++ 01_special_var.sh
@@ -1,2 +1,13 @@
 #! /bin/bash
 echo "All variables passed to script:$@".
+echo "No .of variables passed:$#"
+echo "first variable:$1"
+echo "script name:$0"
+echo "who is runnig this:$USER"
+echo "which directory:$PWD"
+echo "Home directory:$HOME"
+echo "PID of the current script:$$"
+echo "line number:$LINENO"
+echo "running time in seconds:$SECONDS"
+echo "random number:$RANNDOM"
+echo "it checks previes command:$?"
