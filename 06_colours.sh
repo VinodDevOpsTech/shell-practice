ac7f79a6d14a0a5896ff755360b56358d61f8c06
VinodDevOpsTech <vinodkumar160107@gmail.com>
2026-05-25 19:02:47 +0530

    hello

---
+++ 06_colours.sh
@@ -0,0 +1,34 @@
+USERID=$(id -u)
+
+LOG_DIR=/var/log/shell-script
+LOG_FILE=$LOG_DIR/$(basename $0).log
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[31m"
+if [ $USERID -ne 0 ]; then
+    echo "ERROR: please run this in root user"
+    exit 1
+fi
+
+FUNCTION(){
+    if [ $2 -ne 0 ]; then
+        echo -e "$TIMESTAMP [ERROR] installing $2....$R FAILED" $N | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$TIMESTAMP [INFO] installing $2....$G SUCCESS" $N | tee -a $LOG_FILE
+    fi
+}
+
+for pack in $@
+do
+    echo "$TIMESTAMP [INFO] installing $pack"
+    dnf list installed $pack &>> $LOG_FILE
+    if [ $? -ne 0 ]; then
+        dnf install $pack -y &>> $LOG_FILE
+        FUNCTION pack $?
+    else
+        echo -e "$TIMESTAMP [INFO] $pack is already installed....$Y SKIPPING $N"
+    fi
+done
