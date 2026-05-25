8ef63fc8051069e60cab5d6fa1a715375f02b69d
VinodDevOpsTech <vinodkumar160107@gmail.com>
2026-05-25 19:07:44 +0530

    hello

--- 06_colours.sh
+++ 06_colours.sh
@@ -6,7 +6,7 @@ TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
-N="\e[31m"
+N="\e[0m"
 if [ $USERID -ne 0 ]; then
     echo "ERROR: please run this in root user"
     exit 1
@@ -14,10 +14,10 @@ fi
 
 FUNCTION(){
     if [ $2 -ne 0 ]; then
-        echo -e "$TIMESTAMP [ERROR] installing $2....$R FAILED" $N | tee -a $LOG_FILE
+        echo -e "$TIMESTAMP [ERROR] installing $2....$R FAILED$N" | tee -a $LOG_FILE
         exit 1
     else
-        echo -e "$TIMESTAMP [INFO] installing $2....$G SUCCESS" $N | tee -a $LOG_FILE
+        echo -e "$TIMESTAMP [INFO] installing $2....$G SUCCESS$N" | tee -a $LOG_FILE
     fi
 }
 
@@ -29,6 +29,6 @@ do
         dnf install $pack -y &>> $LOG_FILE
         FUNCTION pack $?
     else
-        echo -e "$TIMESTAMP [INFO] $pack is already installed....$Y SKIPPING $N"
+        echo -e "$TIMESTAMP [INFO] $pack is already installed....$Y SKIPPING$N"
     fi
 done
