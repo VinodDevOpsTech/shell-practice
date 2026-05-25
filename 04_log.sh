032565037e38eb0515f8a75815ad6696145e517d
VinodDevOpsTech <vinodkumar160107@gmail.com>
2026-05-25 16:26:48 +0530

    hello

--- 04_log.sh
+++ 04_log.sh
@@ -1,36 +1,29 @@
-
-
-#!/bin/bash
-
 USERID=$(id -u)
-LOG_DIR=/home/ec2-user/shell-log
-LOG_FILE=$LOG_DIR/$0.log
+
+LOG_DIR=/var/log/shell-script
+LOG_FILE=$LOG_DIR/$(basename $0).log
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR: please run this in root user"
     exit 1
 fi
 
-#first arg -> What are you trying to install
-#second arg -> Exit Code
-
 FUNCTION(){
-    if [ $? -ne 0 ]; then
-        echo "installing $1....FAILED"
+    if [ $1 -ne 0 ]; then
+        echo "installing $2....FAILED"
         exit 1
-
     else
-        echo "installing $1....SUCCESS!!!"
+        echo "installing $2....SUCCESS!!!"
     fi
 }
 
-dnf list installed mysql -y &>>$LOG_FILE
+dnf list installed mysql &>>$LOG_FILE
+
 if [ $? -eq 0 ]; then
-    echo "mySQL is already installed.... SKIPPING"
-    exit 1
-else    
+    echo "mySQL is already installed....SKIPPING"
+    exit 0
+else
     echo "installing mySQL"
     dnf install mysql -y &>>$LOG_FILE
-    FUNCTION MySQL $?
-    
-fi    
+    FUNCTION $? MySQL
+fi
